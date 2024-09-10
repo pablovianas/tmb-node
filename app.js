3d1c1a4c8c6ebe7e507eb46062c5b24f3e21f99a
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2024-09-10 19:33:33 -0300

    Adiciona Dockerfile

--- app.js
+++ app.js
@@ -20,9 +20,20 @@ const app = express();
 
 app.get('/tmb', async (req, res) => {
   let browser;
+  // Inicia o browser
+  browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   try {
-    // Inicia o browser
-    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
     // Navega até a página de login
