37ba041f2c47bf835535c3faeadfd28ec2ae356e
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2024-09-28 16:22:48 -0300

    Adiciona endpoint de obter token e remove logs

--- app.js
+++ app.js
@@ -42,16 +42,12 @@ app.get('/tmb', async (req, res) => {
 
     await sleep(3000);
 
-    await page.screenshot({ path: 'tmb.png', fullPage: true })
-
-
     // Preenche o formulário de login
     await page.type('#login', process.env.TMB_USER);
 
     sleep(5000);
-    await page.screenshot({ path: 'tmb2.png', fullPage: true })
-    await page.type('#senha', process.env.TMB_PASS);
 
+    await page.type('#senha', process.env.TMB_PASS);
 
     //Submete o formulário
     await Promise.all([
@@ -77,8 +73,6 @@ app.get('/tmb', async (req, res) => {
     );
 
     await sleep(10000);
-    await page.screenshot({ path: 'tmb4.png', fullPage: true })
-
 
     // Fecha o browser
     await browser.close();
@@ -94,6 +88,58 @@ app.get('/tmb', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+app.get('/obter-token', async (req, res) => {
+
+  let browser;
+  // Inicia o browser
+  browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
+
+    // Navega até a página de login
+    await page.goto('https://produtor.tmbeducacao.com.br/', { waitUntil: 'networkidle2' });
+
+    await sleep(5000);
+
+    // Preenche o formulário de login
+    await page.type('#login', process.env.TMB_USER);
+
+    sleep(5000);
+    await page.type('#senha', process.env.TMB_PASS);
+
+    //Submete o formulário
+    await Promise.all([
+      page.click("body > main > section > div.lg\\:w-1\\/2.w-full.bg-tmb-preto.shadow-lg.shadow-inherit.px-0.z-0.h-auto.overflow-hidden > div > div.flex.flex-col.justify-evenly.flex-1 > div:nth-child(2) > div > div:nth-child(2) > form > div.pb-2.pt-10.md\\:pt-20.flex.flex-col > button > div > div"),
+      page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+
+    const cookies = await page.cookies();
+
+    cookies.filter(cookie => cookie.name === 'tmb.token').map(cookie => {
+      res.status(200).json({ token: cookie.value });
+    });
+
+  
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+  finally {
+    if (browser) await browser.close();
+  }
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
